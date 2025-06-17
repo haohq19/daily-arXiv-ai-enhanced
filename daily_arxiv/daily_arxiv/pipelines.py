3bdba07db1ca42b68310e192bd24ce5b13b0c10a
haohq19 <haohq19@gmail.com>
2025-06-18 00:32:17 +0800

    add keyword filter to arxiv spider pipeline

--- daily_arxiv/daily_arxiv/pipelines.py
+++ daily_arxiv/daily_arxiv/pipelines.py
@@ -5,14 +5,25 @@
 
 
 # useful for handling different item types with a single interface
+import re
+import os
 import arxiv
-
+from scrapy.exceptions import DropItem
 
 class DailyArxivPipeline:
     def __init__(self):
         self.page_size = 100
         self.client = arxiv.Client(self.page_size)
+        keywords = os.environ.get("KEYWORDS", "cs.CV")
+        keywords = keywords.split(",")
+        keywords = set(map(str.strip, keywords))   # keywords to filter papers, e.g.. {"event", "dvs", "neuromorphic"}
+        self.keyword_pattern = re.compile('|'.join(map(re.escape, keywords)), re.IGNORECASE)
 
+    def check_keywords(self, text: str) -> bool:
+        if not text:
+            return False
+        return bool(self.keyword_pattern.search(text))
+    
     def process_item(self, item: dict, spider):
         item["pdf"] = f"https://arxiv.org/pdf/{item['id']}"
         item["abs"] = f"https://arxiv.org/abs/{item['id']}"
@@ -25,5 +36,11 @@ class DailyArxivPipeline:
         item["categories"] = paper.categories
         item["comment"] = paper.comment
         item["summary"] = paper.summary
-        print(item)
+        # print(item)
+
+        if (not self.check_keywords(item["summary"])) and (not self.check_keywords(item["title"])):
+            spider.logger.debug(f"Skipped paper {item['id']} - {item['title']}")
+            raise DropItem(f"No keywords matched")
+
+        spider.logger.info(f"Processed paper {item['id']} - {item['title']}")
         return item
